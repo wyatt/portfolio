d06b377d5229d687864033c3e5ac0e19d5635cdb
Wyatt Sell <profwyattb@gmail.com>
2021-04-01 19:08:11 +0100

    Getting to grips with Next.js 🐋

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import {AppProps} from "next/app";
+import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import { GlobalStyles } from "../components/GlobalStyles";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
-export default function Home() {
-  return (
-    <div className="max-w-7xl mx-auto">Hello, Tailwind!</div>
-  )
-}
+import React, { ReactNode } from "react";
+import tw, { css, styled, theme } from "twin.macro";
+
+const Home = () => {
+  return <div tw="max-w-7xl mx-auto">Hello!</div>;
+};
+
+export default Home;
