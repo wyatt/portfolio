f16f4bf098b5f1c4eb62fa8510e66dd740e1c4ff
Wyatt Sell <profwyattb@gmail.com>
2021-04-05 12:54:13 +0100

    Changed url to heroku

--- helper/fetcher.ts
+++ helper/fetcher.ts
@@ -1,5 +1,5 @@
 export const fetcher = async (route: string) => {
-  return await fetch(`http://localhost:8080${route}`)
+  return await fetch(`https://wyatt-portfolio.herokuapp.com${route}`)
     .then((res) => res.json())
     .catch((e) => {
       return false;
