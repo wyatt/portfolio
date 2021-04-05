d4e5aeae3e85b21216acef4422338f6b77e4e6e9
Wyatt Sell <profwyattb@gmail.com>
2021-04-05 11:45:10 +0100

    SEO stuff

--- pages/_document.js
+++ pages/_document.js
@@ -28,13 +28,16 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta charset="UTF-8" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <title>Wyatt Sell</title>
+          <meta name="description" content="Frontend developer" />
         </Head>
         <body>
           <Main />
