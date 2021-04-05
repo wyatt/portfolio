c717da631b711b3d94f431754e6738348a5077a8
Wyatt Sell <profwyattb@gmail.com>
2021-04-05 12:03:02 +0100

    Changes to fetcher function

--- helper/fetcher.ts
+++ helper/fetcher.ts
@@ -1,10 +1,8 @@
 export const fetcher = async (route: string) => {
-  try {
-    const response = await fetch(`http://localhost:8080${route}`).then((res) =>
-      res.json()
-    );
-    return response;
-  } catch (e) {
-    return false;
-  }
+  return await fetch(`http://localhost:8080${route}`)
+    .then((res) => res.json())
+    .catch((e) => {
+      console.log(e);
+      return false;
+    });
 };
