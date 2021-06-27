a70e82f9c4cf85b5a30f3ca9e6637fe8e91181e8
Wyatt Sell <profwyattb@gmail.com>
2021-06-27 22:07:09 +0100

    Fixed git conflict

--- next.config.js
+++ next.config.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       // Unset client-side javascript that only works server-side
-//       // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
-//       config.node = { fs: "empty", module: "empty" };
-//     }
-//
-//     return config;
-//   },
-//   images: {
-//     domains: ["i.scdn.co"],
-//   },
-// };
-=======
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -28,4 +12,3 @@ module.exports = {
     domains: ["i.scdn.co"],
   },
 };
->>>>>>> c8b62f7f3720554f8f8c230f487f3df91eea83af
