e1c58f3e720b43865deb9fdfcc28d5fa76db5596
Wyatt Sell <profwyattb@gmail.com>
2021-06-27 22:11:44 +0100

    Is there anything git can't mess up

--- helper/fetcher.ts
+++
@@ -1,7 +0,0 @@
-export const fetcher = async (route: string) => {
-  return await fetch(`https://wyatt-portfolio.herokuapp.com${route}`)
-    .then((res) => res.json())
-    .catch((e) => {
-      return false;
-    });
-};
